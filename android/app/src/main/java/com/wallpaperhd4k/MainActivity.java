8d55081778c057e83beae0e014aa53b13ae4f0cd
Ninh Tran <ninh.tran@sotatek.com>
2020-07-06 14:03:36 +0700

    update splashscreen cnt

--- android/app/src/main/java/com/wallpaperhd4k/MainActivity.java
+++ android/app/src/main/java/com/wallpaperhd4k/MainActivity.java
@@ -1,6 +1,11 @@
 package com.wallpaperhd4k;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,21 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "wallpaperhd4k";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); // here
+    super.onCreate(savedInstanceState);
   }
 }
--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,39 +1,16 @@
-import React, {useEffect} from 'react';
-import {View} from 'react-native';
+import {useEffect} from 'react';
 import {useNavigation} from 'react-navigation-hooks';
-import firestore from '@react-native-firebase/firestore';
-import {useDispatch} from 'react-redux';
 import {screenNames} from '../configs/const';
-import {setApiKeys, setCollections} from '../redux/master';
 
 const SplashScreen = () => {
-  const dispatch = useDispatch();
   const {navigate} = useNavigation();
 
   useEffect(() => {
-    let unsubscribe1;
-    const unsubscribe = firestore()
-      .collection('collections')
-      .onSnapshot(querySnapshot => {
-        const collections = querySnapshot.docs.map(documentSnapshot => ({
-          search: documentSnapshot.data()?.search,
-          image: documentSnapshot.data()?.image,
-        }));
-        dispatch(setCollections(collections));
-
-        unsubscribe1 = firestore()
-          .collection('apikeys')
-          .onSnapshot(querySnapshot1 => {
-            const apiKeys = querySnapshot1.docs.map(
-              documentSnapshot => documentSnapshot.data()?.apiKey,
-            );
-            dispatch(setApiKeys(apiKeys));
-            goMainScreen();
-          });
-      });
+    const timeout = setTimeout(() => {
+      goMainScreen();
+    }, 1000);
     return () => {
-      unsubscribe();
-      unsubscribe1();
+      clearTimeout(timeout);
     };
   }, []);
 
@@ -41,9 +18,7 @@ const SplashScreen = () => {
     navigate({routeName: screenNames.MainScreen, params: {}});
   };
 
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
-  );
+  return null;
 };
 
 export default SplashScreen;
